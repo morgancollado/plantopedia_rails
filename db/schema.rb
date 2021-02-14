01de41ea68166d7867a73b2373f9d588daff892d
Morgan Collado <morgan.collado@gmail.com>
2021-02-14 13:45:25 -0600

    adds ImgUrl to plants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_165413) do
+ActiveRecord::Schema.define(version: 2021_02_14_194418) do
 
   create_table "owned_plants", force: :cascade do |t|
     t.integer "user_id"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_10_20_165413) do
     t.string "scientific_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "img_url"
   end
 
   create_table "users", force: :cascade do |t|
